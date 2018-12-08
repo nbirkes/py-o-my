7db6213bfd63d9239c4d8046ae05acc89f093395
Nathan Birkes <nathan.birkes@veracity.vision>
2018-12-08 16:20:20 -0600

    Formatting

--- dog.py
+++ dog.py
@@ -2,13 +2,13 @@ class Dog:
 
     def __init__(self, name):
         self.name = name
-        self.tricks = []    # creates a new empty list for each dog
+        self.tricks = []  # creates a new empty list for each dog
 
     def add_trick(self, trick):
         self.tricks.append(trick)
+
 
 cosette = Dog('Cosette')
 cosette.add_trick('bark')
 cosette.add_trick('play dead')
 print(cosette.tricks)
-
